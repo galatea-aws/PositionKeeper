d5bfde26943643e1e32b2f7b1ff7c6852141755f
galatea-aws <aws@galatea-associates.com>
2014-01-28 11:25:25 -0500

    test

--- src/PositionKeeper/CountTradesByAccountTester.java
+++ src/PositionKeeper/CountTradesByAccountTester.java
@@ -32,7 +32,7 @@ public class CountTradesByAccountTester extends VoltPerformanceTester{
     	
     	String queryDuration = String.valueOf((double)(System.currentTimeMillis()-queryStartTS)/1000f);
         while(result.advanceRow()) {
-            String output = "CountTradesByAccount," + queryDuration + "," + result.getRowCount() + "," + CountTradesByAccount.resultStmt.getText();
+            String output = "CountTradesByAccount," + queryDuration + "," + result.getLong(0) + "," + CountTradesByAccount.resultStmt.getText();
             System.out.println(output);
         }
 
