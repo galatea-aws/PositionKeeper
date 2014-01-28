eeb4349efe35038414c18949210a7db5a7883fe1
galatea-aws <aws@galatea-associates.com>
2014-01-28 14:44:17 -0500

    test

--- ddl.sql
+++ ddl.sql
@@ -51,6 +51,7 @@ CREATE PROCEDURE FROM CLASS PositionKeeper.procedures.DoTrade;
 CREATE PROCEDURE FROM CLASS PositionKeeper.procedures.CountTradesByAccount;
 CREATE PROCEDURE FROM CLASS PositionKeeper.procedures.SumPositionByAccount;
 CREATE PROCEDURE FROM CLASS PositionKeeper.procedures.SumPositionByAccountAndProduct;
+CREATE PROCEDURE FROM CLASS PositionKeeper.procedures.SumPositionForAccountGroupByProduct;
 
 -- parition table
 PARTITION TABLE trades ON COLUMN account_id;
